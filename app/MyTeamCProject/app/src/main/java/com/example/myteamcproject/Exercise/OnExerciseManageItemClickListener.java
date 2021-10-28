17bf4a8f76acff2fa3eba94fd3d1754c1c26ccbf
DESKTOP-ATSLSEN\lcocal <youngmoon525@naver.com>
2021-10-28 09:07:48 +0900

    del

--- app/MyTeamCProject/app/src/main/java/com/example/myteamcproject/Exercise/OnExerciseManageItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.myteamcproject.Exercise;
-
-import android.view.View;
-
-public interface OnExerciseManageItemClickListener {
-
-    public void onItemClick(ExerciseManageAdapter.ViewHolder holderm,
-                            View view, int position);
-}
--- app/MyTeamCProject/app/src/main/java/com/example/myteamcproject/Gift/CartWriterClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.myteamcproject.Gift;
-
-import android.view.View;
-
-public interface CartWriterClickListener {
-
-    public void onItemClick(CartAdapter.ViewHolder holderc,
-                            View view, int position);
-}
